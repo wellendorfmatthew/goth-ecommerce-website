69dbc848b8560a36102809fa12f56e6d9d9a6306
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2024-03-11 03:50:07 -0700

    Started implementing UI for order history

--- ecommerce-frontend/src/Sections.jsx
+++ ecommerce-frontend/src/Sections.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './Components/Navbar'
 import { useParams } from 'react-router-dom'
 import { useCart } from './CartContext'
+import MAGIC_WAND from './assets/magic-wand.png';
 
 const Sections = () => {
     const { id } = useParams(); // Allows to set an id to use for fetch request
@@ -30,6 +31,36 @@ const Sections = () => {
         getItem();
     }, []); // Activates upon startup
 
+    const addToWishList = async (name, price, image, id) => {
+        const item = { name: name, id: id, image: image, price: price }
+        try {
+            const wishlist = await fetch(`http://localhost:4015/user/wishlist`, {
+                method: "PUT",
+                body: JSON.stringify({
+                    item: item
+                }),
+                credentials: 'include',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Credentials": "true",
+                    "Access-Control-Allow-Origin": "http://localhost:5173",
+                },
+            });
+
+            const response = await wishlist.json();
+
+            if (response.error) {
+                throw Error(response.error)
+            }
+
+            console.log("wishlist success ", response);
+            return response;
+        } catch (error) {
+            console.log(error.message);
+            return false;
+        }
+    }
+
     return (  
         <>
             <Navbar />
@@ -41,6 +72,7 @@ const Sections = () => {
                     <div className="item-info">
                         <h1 className='sections-header'>{item.name}</h1>
                         <p className='sections-price'>${item.price}</p>
+                        <button className='add-to-wishlist-button' onClick={() => addToWishList(item.name, item.price, item.image, item._id)}><img src={MAGIC_WAND} alt="" className='wishlist-icon' /><p className='wishlist-text'>Add to Wishlist</p></button>
                         <button className='add-cart-button' onClick={() => addToCart(item, 1)}>Add to Cart</button>
                     </div>
                 </div>
